914c589b2d33abf02f76578017db93c708e37fac
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2024-07-29 09:12:24 +0200

    Fixed card display

--- src/app/Components/tool-tip/tool-tip.component.ts
+++ src/app/Components/tool-tip/tool-tip.component.ts
@@ -1,12 +1,40 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tool-tip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tool-tip.component.html',
   styleUrl: './tool-tip.component.scss'
 })
 export class ToolTipComponent {
   @Input() card: string = "";
+  @Input() type: string = "";
+
+  getStyle(): any{
+
+    let style: { [key: string]: string };
+
+    switch (this.type){
+
+      case "field":
+        return  style = {
+          'position': 'absolute',
+          'left' : '10em'
+        };
+
+      case "hand":
+        return  style = {
+          'position': 'absolute',
+          'right' : '-2em',
+          'bottom' : '1em'
+        };
+
+      case "location":
+        return "";
+      default:
+        return ""
+    }
+  }
 }
